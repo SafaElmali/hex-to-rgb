856dc1a58939f4512f913cf845cd1ab1f0e9cddc
SafaElmali <tsafaelmali@gmail.com>
2019-11-10 16:06:46 +0300

    updated component styles && removed package.json-lock

--- src/components/PrevSearches.js
+++ src/components/PrevSearches.js
@@ -4,13 +4,13 @@ import { DisplayRgb } from './DisplayRgb';
 export const PrevSearches = (props) =>
     (
         <div className="card">
-            <div className="card-header text-center header-text-props">
-                Your previous searches <span role="img" aria-label="emoji">👇</span>
+            <div className="card-header text-center card-text-props">
+                Your previous searches will be displayed in here <span role="img" aria-label="emoji">👇</span>
             </div>
             <div className="card-body prev-card-props">
                 {props.searchList.length > 0 ? props.searchList.map((value, index) =>
                     <DisplayRgb isPrev={true} red={value.r} green={value.g} blue={value.b} key={index} />
-                ) : 'null'
+                ) : <span className="card-text-props">Currently, you don't have any previous search.</span>
                 }
             </div>
         </div>
