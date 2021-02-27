bf33fec2e2321416f7e43c160fa5bf41dfb6899a
SafaElmali <tsafaelmali@gmail.com>
2021-02-27 15:04:06 +0300

    refactor: modify styles

--- src/components/DisplayRgb.js
+++ src/components/DisplayRgb.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import { copyToClipboard } from './Toaster';
+import React from "react";
+import { copyToClipboard } from "./Toaster";
 
 export const DisplayRgb = ({ color }) => {
-    const handleCopyRgb = () => {
-        navigator.clipboard.writeText(`rgb(${color.red},${color.green},${color.blue})`);
-        copyToClipboard();
-    }
+  const handleCopyRgb = () => {
+    navigator.clipboard.writeText(
+      `rgb(${color.red},${color.green},${color.blue})`
+    );
+    copyToClipboard();
+  };
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <div className="card mt-3 mb-3" style={{ width: '18rem', borderRadius: '25px', filter: 'drop-shadow(2px 4px 4px black)' }}>
-                <div className="card-header text-center card-title-props" style={{ color: `rgb(${color.red},${color.green},${color.blue})` }}>
-                    <span>rgb({color.red},{color.green},{color.blue})</span>
-                    <button onClick={handleCopyRgb} className="copy-button mt-2">Copy this rgb</button>
-                </div>
-                <div className="card-body">
-                    <p className="text-center card-text-props">Color preview:</p>
-                    <div style={{ width: 50, height: 50, backgroundColor: `rgb(${color.red},${color.green},${color.blue})`, margin: 'auto', border: '3px solid gray' }}>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div
+        className="card mt-3 mb-3"
+        style={{
+          width: "18rem",
+          borderRadius: "25px",
+          boxShadow:"2px 2px 2px 2px lightgray"
+        }}
+      >
+        <div
+          className="card-header text-center card-title-props"
+          style={{ color: `rgb(${color.red},${color.green},${color.blue})` }}
+        >
+          <span>
+            rgb({color.red},{color.green},{color.blue})
+          </span>
+          <button onClick={handleCopyRgb} className="copy-button mt-2">
+            Copy this rgb
+          </button>
         </div>
-    )
-}
+        <div className="card-body">
+          <p className="text-center card-text-props">Color preview:</p>
+          <div
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: `rgb(${color.red},${color.green},${color.blue})`,
+              margin: "auto",
+              borderRadius: "50%",
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,68 @@
-import React, { useEffect, useRef } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import { convertHexToRgb } from '../util';
-import { DisplayRgb } from './DisplayRgb';
-import { GithubCorner } from './GithubCorner';
+import React, { useEffect, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { convertHexToRgb } from "../util";
+import { DisplayRgb } from "./DisplayRgb";
+import { GithubCorner } from "./GithubCorner";
 
 const Header = ({ setValue, value, setColor, setSearchList, color }) => {
-    const input = useRef();
+  const input = useRef();
 
-    useEffect(() => {
-        input.current.focus();
-    }, [])
+  useEffect(() => {
+    input.current.focus();
+  }, []);
 
-    const handleKeyPress = event => event.key === 'Enter' ? handleConvert() : null;
+  const handleKeyPress = (event) =>
+    event.key === "Enter" ? handleConvert() : null;
 
-    const handleConvert = () => {
-        const response = convertHexToRgb(value);
+  const handleConvert = () => {
+    const response = convertHexToRgb(value);
 
-        if (!response) {
-            setValue("");
-        } else {
-            const { red, green, blue } = response;
-            setColor({ red, green, blue });
-            setSearchList(prevList => prevList.concat({ red, green, blue }));
-        }
+    if (!response) {
+      setValue("");
+    } else {
+      const { red, green, blue } = response;
+      setColor({ red, green, blue });
+      setSearchList((prevList) => prevList.concat({ red, green, blue }));
     }
+  };
 
-    return (
-        <>
-            <ToastContainer position={toast.POSITION.TOP_RIGHT} />
-            <GithubCorner />
-            <div className="row">
-                <div className="col-12" style={{ padding: "30px 30px 0px 30px" }}>
-                    <div className="title-props text-center">
-                        <h1>Hex-to-RGB</h1>
-                    </div>
-                    <div className="col-md-4 mx-auto mt-3">
-                        <label className="text-props">Hex Code</label>
-                        <div style={{ position: 'relative' }}>
-                            <i className="fas fa-hashtag icon-props"></i>
-                            <input ref={input} type="text" className="form-control input-props" placeholder="e.g 282C34" maxLength="6" onChange={({ target }) => setValue(target.value)} onKeyPress={handleKeyPress} required />
-                            <i className="fas fa-search search-icon-props" onClick={convertHexToRgb} style={{ cursor: 'pointer' }} id="search"></i>
-                        </div>
-                        <div className="input-group-append">
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <ToastContainer position={toast.POSITION.TOP_RIGHT} />
+      <GithubCorner />
+      <div className="row">
+        <div className="col-12" style={{ padding: "30px 30px 0px 30px" }}>
+          <div className="title-props text-center">
+            <h1>Hex-to-RGB</h1>
+          </div>
+          <div className="col-md-4 mx-auto mt-3">
+            <label className="text-props">Hex Code</label>
+            <div style={{ position: "relative" }}>
+              <i className="fas fa-hashtag icon-props"></i>
+              <input
+                ref={input}
+                type="text"
+                className="form-control input-props"
+                placeholder="e.g 282C34"
+                maxLength="6"
+                onChange={({ target }) => setValue(target.value)}
+                onKeyPress={handleKeyPress}
+                required
+              />
+              <i
+                className="fas fa-search search-icon-props"
+                onClick={() => convertHexToRgb(value)}
+                style={{ cursor: "pointer" }}
+                id="search"
+              ></i>
             </div>
-            {color ? <DisplayRgb color={color} /> : null}
-        </>
-    )
-}
+            <div className="input-group-append"></div>
+          </div>
+        </div>
+      </div>
+      {color ? <DisplayRgb color={color} /> : null}
+    </>
+  );
+};
 
 export default Header;
